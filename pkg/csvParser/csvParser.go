60109a19896bc9c087ac4e095856bb605ee79d26
Tyler Brittain <tbrittain100@gmail.com>
2026-09-15 20:23:12 -0700

    tests: write csv unit tests and stub funcs

--- pkg/csvParser/csvParser.go
+++ pkg/csvParser/csvParser.go
@@ -1,1 +1,50 @@
-package csvparser
+package letterboxdParser
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+)
+
+type movie struct {
+	name string
+	year int
+	url  string
+}
+
+func getMovieList() movie {
+	return movie{}
+}
+
+func getMovie() movie {
+
+	return movie{}
+}
+
+func GetListFromCSV(path string) ([]movie, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return []movie{}, err
+	}
+
+	reader := csv.NewReader(f)
+	records, err := reader.ReadAll()
+	if err != nil {
+		return []movie{}, err
+	}
+
+	for _, record := range records {
+		fmt.Println(record)
+	}
+
+	_ = path
+	return []movie{}, err
+
+}
+
+func hasHeaders(line []string) bool {
+	return false
+}
+func isValid(line []string) bool {
+	return false
+}
---
+++ pkg/csvParser/csvParser_test.go
@@ -0,0 +1,94 @@
+package letterboxdParser
+
+import (
+	"slices"
+	"testing"
+)
+
+const testDir = "samples"
+
+func TestGetMovieList(t *testing.T) {
+	const csvName = "single.csv"
+	const testFile = testDir + "/" + csvName
+
+	expected := []movie{{name: "The Man Who Wasn't There", year: 2001, url: "n/a"}}
+	actual, err := GetListFromCSV(testFile)
+
+	if err != nil {
+		t.Errorf("unexpected error %v encountered", err)
+	}
+
+	if !slices.Equal(expected, actual) {
+		t.Errorf("expected %v, got %v", expected, actual)
+	}
+
+}
+
+// cases
+// single
+// muliple
+// no file
+
+// add get single movies and convert this and bove to table
+
+// helper files
+// []string len: 4, cap: 4, ["Date","Name","Year","Letterboxd URI"]
+func TestHasHeaders(t *testing.T) {
+	type testCase struct {
+		description     string
+		input           []string
+		expectedOuttput bool
+	}
+	tests := []testCase{
+		{
+			description:     "has header",
+			input:           []string{"Date", "Name", "Year", "Letterboxd URI"},
+			expectedOuttput: true,
+		},
+		{
+			description:     "does not have header",
+			input:           []string{"2025-02-09", "Wildflower", "2022", "https://boxd.it/yt94"},
+			expectedOuttput: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.description, func(t *testing.T) {
+			actual := hasHeaders(tt.input)
+			if actual != tt.expectedOuttput {
+				t.Errorf("Recieved %v for line %v", actual, tt.input)
+			}
+		})
+	}
+
+}
+
+func TestIsValid(t *testing.T) {
+	type testCase struct {
+		description     string
+		input           []string
+		expectedOuttput bool
+	}
+	tests := []testCase{
+		{
+			description:     "is not valid(incorrect num)",
+			input:           []string{"Date", "Name", "Letterboxd URI"},
+			expectedOuttput: false,
+		},
+		{
+			description:     "valid",
+			input:           []string{"2025-02-09", "Wildflower", "2022", "https://boxd.it/yt94"},
+			expectedOuttput: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.description, func(t *testing.T) {
+			actual := isValid()
+			if actual != tt.expectedOuttput {
+				t.Errorf("%v marked %v", tt.input, actual)
+			}
+		})
+	}
+
+}
